0447985a1aa6317ffa9ed0d475c461b888f28bdb
Mauro Gestoso <maurogestoso@gmail.com>
2016-05-30 01:53:21 +0100

    Set scale to full height

--- js/Boot.js
+++ js/Boot.js
@@ -10,10 +10,12 @@ HandFoot.Boot.prototype = {
 		this.load.image("lined-paper", "assets/img/lined-paper.png");
 	},
 	create: function () {
-		this.add.tileSprite(0, 0, this.world.width, this.world.height, 'lined-paper');
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 
+		this.add.tileSprite(0, 0, this.world.width, this.world.height, 'lined-paper');
+		
 		this.physics.startSystem(Phaser.Physics.ARCADE);
 		this.physics.arcade.gravity.y = 0;
 
